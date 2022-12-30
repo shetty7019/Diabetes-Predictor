b1e82d1a246118c628e2262b4f80f34c92e375b8
hrishinyk <nayakhriship53@gamil.com>
2022-12-30 23:57:03 +0530

    model fixed

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-import pickle
-from flask import Flask,render_template,request
-
 import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+import sklearn
 
 model = pickle.load(open('model.pkl','rb'))
+dataset = pd.read_csv('diabetes.csv')
 
+dataset_X = dataset.iloc[:,[1, 2, 5, 7]].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0,1))
+dataset_scaled = sc.fit_transform(dataset_X)
 app=Flask(__name__)
 
 @app.route('/')
@@ -14,25 +21,30 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 
-def predict_diabetes():
-    # pregnancies = request.form.get('Pregnancies')
-    glucose = request.form.get('Glucose')
-    # bloodpressure = request.form.get('BloodPressure')
-    # skinthickness = request.form.get('SkinThickness')
-    insulin = request.form.get('Insulin')
-    bmi = request.form.get('BMI')
-    # diabetespedigreefunction = request.form.get('DiabetesPedigreeFunction')
-    age = request.form.get('Age')
+def predict():
+    # # pregnancies = request.form.get('Pregnancies')
+    # glucose = request.form.get('Glucose')
+    # # bloodpressure = request.form.get('BloodPressure')
+    # # skinthickness = request.form.get('SkinThickness')
+    # insulin = request.form.get('Insulin')
+    # bmi = request.form.get('BMI')
+    # # diabetespedigreefunction = request.form.get('DiabetesPedigreeFunction')
+    # age = request.form.get('Age')
 
-    #prediction
+    # #prediction
 
-    result=model.predict(np.array([glucose,insulin,bmi,age]).reshape(1,4))
+    # result=model.predict(np.array([glucose,insulin,bmi,age]).reshape(1,4))
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
+    prediction = model.predict( sc.transform(final_features) )
 
-    if result[0]==1:
-     result='Diabetic'
-    else:
-     result='Not Diabetic'
-    return render_template('index.html',result=result)
+    if prediction == 1:
+        pred = "You have Diabetes, please consult a Doctor."
+    elif prediction == 0:
+        pred = "You don't have Diabetes."
+    output = pred
+
+    return render_template('index.html', prediction_text='{}'.format(output))
 
 
 if __name__=='__main__':
